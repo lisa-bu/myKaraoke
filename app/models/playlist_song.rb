602a0d6b5e449638288a829fa5c1f7e7237e51c1
Haris Mir <idiotbox404@gmail.com>
2025-11-25 12:55:52 +0900

    associations and validations

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,2 +1,7 @@
 class PlaylistSong < ApplicationRecord
+  belongs_to :playlist
+  belongs_to :song
+
+  validates :position, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :playlists
+  has_many :playlists_songs
+  has_many :difficulty_ratings
+  has_many :songs, through: :difficulty_ratings
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Friendships (self-join)
+  has_many :sent_friendships,
+           class_name: "Friendship",
+           foreign_key: "asker_id"
+
+  has_many :received_friendships,
+           class_name: "Friendship",
+           foreign_key: "receiver_id"
+
 end

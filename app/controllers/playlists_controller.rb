e7d319765c4e13fd2faf0eedb3be61ac445bf2fd
Matt McLuckie <mcluckiematt@gmail.com>
2025-11-25 15:42:17 +0900

    Add policy files

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,14 +3,18 @@ class PlaylistsController < ApplicationController
   end
 
   def show
+    authorize @playlist
   end
 
   def create
+    authorize @playlist
   end
 
   def update
+    authorize @playlist
   end
 
   def destroy
+    authorize @playlist
   end
 end

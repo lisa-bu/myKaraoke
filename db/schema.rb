885bc7d0f82c93923006ad636ccd6edcbb8f59bb
Alesya Sewell <alesyadb@icloud.com>
2025-11-25 14:22:17 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_25_021836) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_25_041411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,22 +33,22 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_021836) do
     t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
   end
 
+  create_table "playlist_songs", force: :cascade do |t|
+    t.bigint "playlist_id", null: false
+    t.bigint "song_id", null: false
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
+    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
+  end
+
   create_table "playlists", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_playlists_on_user_id"
-  end
-
-  create_table "playlists_songs", force: :cascade do |t|
-    t.bigint "playlist_id", null: false
-    t.bigint "song_id", null: false
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["playlist_id"], name: "index_playlists_songs_on_playlist_id"
-    t.index ["song_id"], name: "index_playlists_songs_on_song_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_021836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "current_playlist_id"
+    t.string "name"
     t.index ["current_playlist_id"], name: "index_users_on_current_playlist_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -79,8 +80,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_021836) do
   add_foreign_key "difficulty_ratings", "users"
   add_foreign_key "friendships", "users", column: "asker_id"
   add_foreign_key "friendships", "users", column: "receiver_id"
+  add_foreign_key "playlist_songs", "playlists"
+  add_foreign_key "playlist_songs", "songs"
   add_foreign_key "playlists", "users"
-  add_foreign_key "playlists_songs", "playlists"
-  add_foreign_key "playlists_songs", "songs"
   add_foreign_key "users", "playlists", column: "current_playlist_id"
 end

24e7a2e806121542c3e8c24b35a15cc90dbaf64b
Haris Mir <idiotbox404@gmail.com>
2025-11-27 11:32:38 +0900

    carousel complete

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -36,35 +36,3 @@ class PlaylistSongsController < ApplicationController
   end
 
 end
-
-
-
-# class PlaylistsSongsController < ApplicationController
-#   before_action :set_playlist
-#   before_action :set_playlist_song, only: [:destroy]
-
-#   def create
-#     @playlist_song = PlaylistsSong.new(playlist_song_params)
-#     @playlist_song.playlist = @playlist
-
-#     if @playlist_song.save
-#       redirect_to @playlist, notice: "Song added!"
-#     else
-#       render :new, status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def set_playlist
-#     @playlist = Playlist.find(params[:playlist_id])
-#   end
-
-#   def set_playlist_song
-#     @playlist_song = PlaylistsSong.find(params[:id])
-#   end
-
-#   def playlist_song_params
-#     params.require(:playlists_song).permit(:song_id, :position)
-#   end
-# end

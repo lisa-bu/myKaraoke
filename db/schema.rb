84af16f1b46221e64d504a85116466c103642cda
Alesya Sewell <alesyadb@icloud.com>
2025-12-01 17:13:48 +0900

    added spotify_id to our songs schema and changed the btn in landing scss to landing_btn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_28_060502) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_01_075159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_28_060502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "spotify_id"
   end
 
   create_table "users", force: :cascade do |t|

76227601b6d266a7ca3ef8f2514c826aafb02c97
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-02 09:48:47 +0900

    Fully import user spotify playlists and songs now

--- app/services/spotify_client.rb
+++ app/services/spotify_client.rb
@@ -22,29 +22,19 @@ class SpotifyClient
   def user_for(current_user)
     return nil unless current_user.spotify_access_token
 
-    refresh_user_token_if_needed(current_user)
-
     RSpotify::User.new(
       "id" => current_user.spotify_uid,
       "credentials" => {
         "token"         => current_user.spotify_access_token,
         "refresh_token" => current_user.spotify_refresh_token,
-        "expires_at"    => current_user.spotify_expires_at.to_i
+        "expires_at"    => current_user.spotify_expires_at.to_i,
+        "access_refresh_callback" => proc { |new_token, expires_in|
+          current_user.update!(
+            spotify_access_token: new_token,
+            spotify_expires_at: Time.current + expires_in.to_i.seconds
+          )
+        }
       }
-    )
-  end
-
-  # -----------------------------
-  # Auto-refresh token when expired
-  # -----------------------------
-  def refresh_user_token_if_needed(user)
-    return unless user.spotify_expires_at <= Time.current
-
-    refreshed = RSpotify::User.refresh_token(user.spotify_refresh_token)
-
-    user.update!(
-      spotify_access_token: refreshed["access_token"],
-      spotify_expires_at:   Time.current + refreshed["expires_in"].to_i.seconds
     )
   end
 
@@ -73,5 +63,28 @@ class SpotifyClient
   # Need to test lol
   def search_playlists(query, limit: 10)
     RSpotify::Playlist.search(query, limit: limit)
+  end
+
+  # -----------------------------
+  # User Playlist Methods
+  # -----------------------------
+
+  # Fetch all playlists for the authenticated user (public and private)
+  def user_playlists(current_user, limit: 50)
+    spotify_user = user_for(current_user)
+    return [] unless spotify_user
+
+    spotify_user.playlists(limit: limit)
+  end
+
+  # Fetch tracks from a specific Spotify playlist
+  def playlist_tracks(playlist_id, current_user, limit: 100)
+    spotify_user = user_for(current_user)
+    return [] unless spotify_user
+
+    playlist = RSpotify::Playlist.find_by_id(playlist_id)
+    return [] unless playlist
+
+    playlist.tracks(limit: limit)
   end
 end

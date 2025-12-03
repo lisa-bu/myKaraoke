4b4d490fd689509e31e38007615b41367d370822
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-03 17:53:44 +0900

    user agent updates

--- app/services/genius_client.rb
+++ app/services/genius_client.rb
@@ -87,7 +87,9 @@ class GeniusClient
 
   def browser_headers
     {
-      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36", "Accept-Language" => "en"
+      "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language" => "en-US,en;q=0.5"
     }
   end
 

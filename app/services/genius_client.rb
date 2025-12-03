5cf69cc05b09461906cade74db8c7652f2068066
Matt McLuckie <mcluckiematt@gmail.com>
2025-12-03 17:27:09 +0900

    Changed user agent for scraping on heroku

--- app/services/genius_client.rb
+++ app/services/genius_client.rb
@@ -87,7 +87,7 @@ class GeniusClient
 
   def browser_headers
     {
-      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"
+      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36", "Accept-Language" => "en"
     }
   end
 
